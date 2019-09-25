03b6c43ef3c7b0d39ee08b61b7cb5212af28f6e9
Ralf Northman <ralf.northman@bredband.net>
2019-09-25 17:24:42 +0200

    Rewrite with a more elegant solution and proper http error codes.

--- src/main.rs
+++ src/main.rs
@@ -11,64 +11,59 @@ extern crate diesel_rocket;
 
 use self::diesel_rocket::*;
 use self::models::*;
-use diesel::prelude::*;
+use diesel::result::Error as DieselError;
 use rocket_contrib::databases::diesel::MysqlConnection;
 use rocket_contrib::json::{Json, JsonError};
 use rocket_cors::{CorsOptions, Error};
-use std::fmt::Display;
 
-type RocketResult<T> = Json<Result<T, String>>;
+type InResult<'a, T> = Result<Json<T>, JsonError<'a>>;
+type OutResult<T> = Result<Json<T>, CustomError>;
 
-fn out<T>(from: QueryResult<T>) -> RocketResult<T> {
-    match from {
-        Ok(payload) => Json(Ok(payload)),
-        Err(error) => {
-            let err = format!("{}", error);
-            Json(Err(err))
-        }
+#[derive(Debug, Responder)]
+enum CustomError {
+    #[response(status = 500, content_type = "plain")]
+    DbError(String),
+    #[response(status = 422, content_type = "plain")]
+    ApiError(String),
+}
+
+impl<'a> From<JsonError<'a>> for CustomError {
+    fn from(error: JsonError<'a>) -> Self {
+        let s = format!("{:?}", error).replace("\\", "");
+        CustomError::ApiError(s)
     }
 }
 
-fn stringify(x: impl Display) -> String {
-    format!("{}", x)
+impl From<DieselError> for CustomError {
+    fn from(error: DieselError) -> Self {
+        let s = format!("{}", error);
+        CustomError::DbError(s)
+    }
 }
 
 #[database("my_db")]
 struct MyDatabase(MysqlConnection);
 
 #[get("/measurements")]
-fn all(conn: MyDatabase) -> RocketResult<Vec<Measurement>> {
-    let v = Measurement::all(&conn);
-    out(v)
+fn all(conn: MyDatabase) -> OutResult<Vec<Measurement>> {
+    let v = Measurement::all(&conn)?;
+    Ok(Json(v))
 }
 
 #[get("/measurements/<id>")]
-fn id(conn: MyDatabase, id: u64) -> RocketResult<Measurement> {
-    let v = Measurement::one(&conn, id);
-    out(v)
+fn id(conn: MyDatabase, id: u64) -> OutResult<Measurement> {
+    let v = Measurement::one(&conn, id)?;
+    Ok(Json(v))
 }
 
 #[post("/measurements", format = "json", data = "<measurement>")]
 fn create(
     conn: MyDatabase,
-    measurement: Result<Json<NewMeasurement>, JsonError>,
-) -> RocketResult<u64> {
-    let result = match measurement {
-        Ok(json) => {
-            json.into_inner().create(&conn).map_err(stringify)
-        }
-        Err(error) => Err(format!("{:?}", error)),
-    };
-    Json(result)
-}
-
-#[post("/two-measurements", format = "json", data = "<measurements>")]
-fn create_two(
-    conn: MyDatabase,
-    measurements: Json<[NewMeasurement; 2]>,
-) -> RocketResult<(u64, u64)> {
-    let v = db_create_two(&conn, measurements.into_inner());
-    out(v)
+    measurement: InResult<NewMeasurement>,
+) -> OutResult<u64> {
+    let insert = measurement?;
+    let last_id = insert.create(&conn)?;
+    Ok(Json(last_id))
 }
 
 fn main() -> Result<(), Error> {
